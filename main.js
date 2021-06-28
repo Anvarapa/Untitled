a2b3cf6eb86f8f381bc719efd6162019ae36ae0a
anvar <turdaliev.anvar11@gmail.com>
2021-06-28 12:57:35 +0300

    untitled

---
+++ main.js
@@ -0,0 +1,39 @@
+/*alert("Hello world")*/
+console.log("sdfgsdg"+ "5" + 1234) //если в ковичках, то js не выщитывает
+//get number
+// x = prompt("введите номер", "+996" )//строка введение данных
+// console.log("ваш номер" , x)
+//
+// x=prompt("X")
+// y=prompt("Y")
+// y=prompt("Y")
+// if(x > 10){
+// console.log("X more then 10")
+// console.log("X = " + x)
+// }
+
+// else{
+//     console.log("x less then 10")
+// }
+// if(x > y){
+//     console.log("x more then y")
+//     console.log("x = " + x)
+//     console.log("y = " + y)
+// }
+// else{
+//     console.log("x less then y")
+// // }
+
+// z = prompt ("Z")
+// w = prompt ("W")
+// p = prompt ("p")
+// if( z > w && z > p){
+//     console.log("z more then w")
+// }
+// else if(w > z && w > p){
+//     console.log("w more then p")
+    
+// }
+// else{
+//     console.log("p more then w,z" )
+// }
